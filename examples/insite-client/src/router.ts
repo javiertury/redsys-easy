7f338ba5e193f19d7b889275ee871b4104a24daa
javiertury <javiertury@gmail.com>
2021-07-14 20:45:01 +0200

    chore: remove unused dependencies
    The package sax was not used and the insite client example was not using effector

--- examples/insite-client/src/router.ts
+++ examples/insite-client/src/router.ts
@@ -1,5 +1,4 @@
 import { createStore } from 'solid-js/store';
-// import { createStore, createEvent } from 'effector';
 
 export type PageName = 'checkout' | 'payment-selection' | 'card-payment' | 'order-summary';
 
