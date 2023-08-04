8653f0c1928ae5a11cf22e498013520a776982b1
javiertury <javiertury@gmail.com>
2023-08-04 15:51:05 +0200

    chore: update dependencies

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,20 +1,25 @@
 import nodeResolve from '@rollup/plugin-node-resolve';
 import typescript from 'rollup-plugin-typescript2';
-import pkg from './package.json';
+import pkg from './package.json' assert { type: 'json' };
 
 /** @type{import('rollup').RollupOptions} */
 const options = {
   input: 'src/index.ts',
-  output: [{
-    file: pkg.main,
-    format: 'cjs'
-  }, {
-    file: pkg.module,
-    format: 'esm'
-  }],
+  output: [
+    {
+      file: pkg.main,
+      format: 'cjs'
+    },
+    {
+      file: pkg.module,
+      format: 'esm'
+    }
+  ],
   plugins: [
     nodeResolve({ resolveOnly: [/^\./] }),
-    typescript({ tsconfig: './tsconfig.build.json' })
+    typescript({
+      tsconfig: './tsconfig.build.json'
+    })
   ]
 };
 
